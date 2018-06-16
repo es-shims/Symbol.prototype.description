9df390cdd942dc70c839766d8ba10c91a57e9e94
Jordan Harband <ljharb@gmail.com>
2018-06-16 14:15:28 -0700

    [Fix] `polyfill`: fix always-true logic
    Fixes #3.

--- polyfill.js
+++ polyfill.js
@@ -9,11 +9,15 @@ module.exports = function descriptionPolyfill() {
 	if (!hasSymbols || typeof gOPD !== 'function') {
 		return null;
 	}
+
 	var desc = gOPD(Symbol.prototype, 'description');
 	if (!desc || typeof desc.get !== 'function') {
 		return implementation;
 	}
-	if (typeof desc.get.call(Symbol()) !== 'undefined' || desc.get.call(Symbol()) !== '' || desc.get.call(Symbol('a')) !== 'a') {
+
+	var emptySymbolDesc = desc.get.call(Symbol());
+	var emptyDescValid = typeof emptySymbolDesc === 'undefined' || emptySymbolDesc === '';
+	if (!emptyDescValid || desc.get.call(Symbol('a')) !== 'a') {
 		return implementation;
 	}
 	return desc.get;
